393817ad879737a1354357f9cad6ed4844a55cc6
adda issa abdoul razak <addaissaabdoulrazak@gmail.com>
2023-03-20 06:57:07 +0100

    added login Form Validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+loginForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email:[
+        '',
+        [Validators.required, Validators.email]
+      ],
+      pwd:[
+        '',
+        [Validators.required, Validators.minLength(6), Validators.maxLength(15)]
+      ]
+    })
   }
 
+  login(){
+
+    // Comming soon....
+  }
+
+//#region
+  get Email():FormControl{
+    return this.loginForm.get('email') as FormControl
+  }
+
+  get PWD() :FormControl{
+    return this.loginForm.get('pwd') as FormControl
+  }
+
+
+//#End region
 }
